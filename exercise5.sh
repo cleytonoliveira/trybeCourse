8e48daa52f095b608ab97a41ba5d68e7dc7c35cb
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-06-30 12:26:16 -0300

    Add exercise 5

---
+++ exercise5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Can you enter with a file or directory path?"
+read FILE
+
+if [ -f $FILE ]
+    then
+        echo "$FILE It's a commom file!"
+elif [ -d $FILE ]
+    then
+    echo "$FILE It's a directory!"
+else
+    echo "$FILE It's another type of file."
+fi
