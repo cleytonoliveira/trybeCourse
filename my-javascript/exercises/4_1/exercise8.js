5594bb908d0b1525b006c39c3c4291d14834baf2
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-07-13 22:11:31 -0300

    Add exercise 8

---
+++ my-javascript/exercises/4_1/exercise8.js
@@ -0,0 +1,9 @@
+const firstNumber = 0;
+const secondNumber = 1;
+const thirdNumber = 2;
+
+if ((firstNumber % 2 == 0) || (secondNumber % 2 == 0) || (thirdNumber % 2 == 0)) {
+    console.log(true);
+} else {
+    console.log(false);
+}
