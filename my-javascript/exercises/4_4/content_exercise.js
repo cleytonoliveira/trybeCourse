863ffda51ccd3a12014e9c05727d1a3f9c2d0cc2
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-07-16 15:14:30 -0300

    Add exercise of the content

---
+++ my-javascript/exercises/4_4/content_exercise.js
@@ -0,0 +1,28 @@
+// Refactoring exercises from 4_1
+// Sum
+function sum(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+console.log(sum(2,2));
+
+// Subtraction
+function subtraction(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+console.log(subtraction(2,2));
+
+// Multiple
+function multiple(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+console.log(multiple(2,2));
+
+// Division
+function division(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+
+console.log(division(2,2));
