2f084f437d2eee96dc2ae26d234f6b73674c4730
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-07-27 06:19:34 -0300

    Refactoring exercise 2

--- my-javascript/exercises/4_4/exercise2.js
+++ my-javascript/exercises/4_4/exercise2.js
@@ -2,7 +2,8 @@ let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-  recorrente: "Sim",
 }
+
+info.recorrente = 'Sim';
 
   console.log(info);
