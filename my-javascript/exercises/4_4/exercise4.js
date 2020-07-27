b0b02b650fd6dfe401d712d15ec571fc810b7c8d
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-07-27 06:23:27 -0300

    Refactoring exercise 4

--- my-javascript/exercises/4_4/exercise4.js
+++ my-javascript/exercises/4_4/exercise4.js
@@ -1,10 +1,11 @@
 let info = {
-    personagem: "Margarida",
-    origem: "Pato Donald",
-    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-    recorrente: "Sim",
-  }
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+info.recorrente = 'Sim';
 
 for (let index in info) {
-    console.log(info[index]);
+  console.log(info[index]);
 }
