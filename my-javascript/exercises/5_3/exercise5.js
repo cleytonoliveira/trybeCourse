eeef1a668c0b87c5d2e83a677e8c882c021f3979
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-07-28 16:02:19 -0300

    Exercise 5 add events on the buttons and input interaction

---
+++ my-javascript/exercises/5_3/exercise5.js
@@ -0,0 +1,37 @@
+var personName = "";
+
+function complainMessage() {
+  alert("I told you not to do that " + personName);
+}
+
+function typeAndScream() {
+  document.getElementById("audio").play();
+  personName = document.getElementById("name").value;
+}
+
+function goDark() {
+  var body = document.getElementsByTagName("body")[0];
+  if (body.className == "") {
+    body.className = "dark";
+  }
+  else {
+    body.className = "";
+  }
+  
+}
+
+function finishTheStory() {
+  document.getElementById("story").innerHTML = "So I carefully searched the damp dungeon. There you are! I grinned a smile with all 98 of my gleaming teeth. Dinner time!";
+}
+
+const btn1 = document.querySelectorAll("button")[0];
+btn1.addEventListener('click', goDark);
+
+const btn2 = document.querySelectorAll("button")[1];
+btn2.addEventListener('click', complainMessage);
+
+const btn3 = document.querySelectorAll("button")[2];
+btn3.addEventListener('click', finishTheStory);
+
+const input1 = document.querySelectorAll("input")[0];
+input1.addEventListener('keyup', typeAndScream);
