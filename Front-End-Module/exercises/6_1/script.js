9ceeba54afb68d852d2233b6a4a882d9be59b9ee
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-08-05 01:09:34 -0300

    Add validation for date

--- Front-End-Module/exercises/6_1/script.js
+++ Front-End-Module/exercises/6_1/script.js
@@ -29,11 +29,30 @@ const states = [
   'Tocantins',
 ];
 
-const selectStates = document.getElementById('states');
-for (let index = 0; index < states.length; index += 1) {
-  const statesOptions = document.createElement('option');
-  selectStates.appendChild(statesOptions);
-  statesOptions.innerHTML = states[index];
-  statesOptions.value = states[index];
+function generateStates() {
+  const selectStates = document.getElementById('states');
+  for (let index = 0; index < states.length; index += 1) {
+    const statesOptions = document.createElement('option');
+    selectStates.appendChild(statesOptions);
+    statesOptions.innerHTML = states[index];
+    statesOptions.value = states[index];
+  }
 }
 
+const date = document.getElementById('date');
+function verifyDate() {
+  const dateValue = date.value;
+  const dateSplit = dateValue.split('/')
+  if (dateSplit[0] > 31 || dateSplit[0] < 1){
+    alert('Data inválida!');
+  }
+  if (dateSplit[1] > 12 || dateSplit[1] < 1){
+    alert('Data inválida!');
+  }
+  if (dateSplit[2] < 0 || (dateSplit[2]).length > 4) {
+    alert('Data inválida!');
+  }
+}
+
+generateStates();
+date.addEventListener('change', verifyDate);
