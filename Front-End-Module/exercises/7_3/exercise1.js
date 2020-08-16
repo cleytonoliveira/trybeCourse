3d75fe4657c613b8bec90f993569bed3d272b5f0
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-08-16 17:40:03 -0300

    Refactoring exercise

--- Front-End-Module/exercises/7_3/exercise1.js
+++ Front-End-Module/exercises/7_3/exercise1.js
@@ -12,4 +12,9 @@ function sum(a, b) {
 assert.strictEqual(typeof(sum), 'function');
 assert.strictEqual(sum(4, 5), 9);
 assert.strictEqual(sum(0, 0), 0);
-assert.strictEqual(sum(4, '5'), 9);
+assert.throws(() => {
+  sum(4, '5');
+});
+assert.throws(() => {
+  sum(4, '5');
+}, /^Error: parameters must be numbers$/);
