6acb8a0763bbf2c4cb3435b407c74ff072802b02
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-03 19:42:57 -0300

    Add describe

--- Front-End-Module/my-jest/exercises/10_1/exercise1.test.js
+++ Front-End-Module/my-jest/exercises/10_1/exercise1.test.js
@@ -1,21 +1,24 @@
 const sum = require('./exercise1');
 
-test('O retorno deve ser igual a 9', () => {
-  expect(sum(4, 5)).toBe(9);
+describe('Testando função Sum', () => {
+  test('O retorno deve ser igual a 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+
+  test('O retorno deve ser igual a 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  test('Retorna erro quando passado uma string', () => {
+    expect(() => {
+      sum(4, '5')
+    }).toThrow(Error);
+  });
+
+  test('Retorna a menssagem parameters must be numbers ao usar string', () => {
+    expect(() => {
+      sum(4, '5')
+    }).toThrow('parameters must be numbers');
+  });
 });
 
-test('O retorno deve ser igual a 0', () => {
-  expect(sum(0, 0)).toBe(0);
-});
-
-test('Retorna erro quando passado uma string', () => {
-  expect(() => {
-    sum(4, '5')
-  }).toThrow(Error);
-})
-
-test('Retorna a menssagem parameters must be numbers ao usar string', () => {
-  expect(() => {
-    sum(4, '5')
-  }).toThrow('parameters must be numbers');
-})
