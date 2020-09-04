4a8bc59ba169c39678ba6d34a55fdc733913a35e
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-03 23:07:07 -0300

    Refactoring

--- Front-End-Module/my-jest/exercises/10_1/exercise1.test.js
+++ Front-End-Module/my-jest/exercises/10_1/exercise1.test.js
@@ -1,21 +1,21 @@
 const sum = require('./exercise1');
 
 describe('Testando função Sum', () => {
-  test('O retorno deve ser igual a 9', () => {
+  it('O retorno deve ser igual a 9', () => {
     expect(sum(4, 5)).toBe(9);
   });
 
-  test('O retorno deve ser igual a 0', () => {
+  it('O retorno deve ser igual a 0', () => {
     expect(sum(0, 0)).toBe(0);
   });
 
-  test('Retorna erro quando passado uma string', () => {
+  it('Retorna erro quando passado uma string', () => {
     expect(() => {
       sum(4, '5')
     }).toThrow(Error);
   });
 
-  test('Retorna a menssagem parameters must be numbers ao usar string', () => {
+  it('Retorna a menssagem parameters must be numbers ao usar string', () => {
     expect(() => {
       sum(4, '5')
     }).toThrow('parameters must be numbers');
