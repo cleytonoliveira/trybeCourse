62ba79384fca56e105fb4ad3e35291af3da1065f
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-16 19:31:45 -0300

    Add a component pokedex and pokemon

--- Front-End-Module/my-react/exercises/11_2/my-pokedex/src/App.js
+++ Front-End-Module/my-react/exercises/11_2/my-pokedex/src/App.js
@@ -1,26 +1,18 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Pokedex from './Pokedex';
+import pokemons from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Pokedex</h1>
+        <Pokedex pokemons={pokemons} />
+      </div>
+    )
+  }
 }
 
 export default App;
