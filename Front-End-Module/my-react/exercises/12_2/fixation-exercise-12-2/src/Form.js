31b858577bfe7e9b57996e13dce4d2a9e182cc4f
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-24 12:33:56 -0300

    Add fieldset and file to update

--- Front-End-Module/my-react/exercises/12_2/fixation-exercise-12-2/src/Form.js
+++ Front-End-Module/my-react/exercises/12_2/fixation-exercise-12-2/src/Form.js
@@ -25,30 +25,43 @@ class Form extends Component {
   render() {
     return (
       <form>
-        <select name="escolhaTeste" value={this.state.escolhaTeste} onChange={this.handleChange}>
-          <option value="teste1">teste 1</option>
-          <option value="teste2">teste 2</option>
-        </select>
-        <label>
-          Nome
-          <input type="text" name="firstname" onChange={this.handleChange} />
-        </label>
-        <label>
-          Idade
-          <input type="number" name="age" onChange={this.handleChange} />
-        </label>
-        <label>
-          Email
-          <input type="email" name="email" onChange={this.handleChange} />
-        </label>
-        <label>
-          Teste agora
-          <input type="checkbox" name="testando" onChange={this.handleChange} />
-        </label>
-        <label>
-          Mensagem
-          <textarea name="message" onChange={this.handleChange} />
-        </label>
+        <fieldset>
+          <legend>Primeira parte</legend>
+          <select name="escolhaTeste" value={this.state.escolhaTeste} onChange={this.handleChange}>
+            <option value="teste1">teste 1</option>
+            <option value="teste2">teste 2</option>
+          </select>
+          <label>
+            Nome
+            <input type="text" name="firstname" onChange={this.handleChange} />
+          </label>
+        </fieldset>
+        <fieldset>
+          <legend>Segunda parte</legend>
+          <label>
+            Idade
+            <input type="number" name="age" onChange={this.handleChange} />
+          </label>
+          <label>
+            Email
+            <input type="email" name="email" onChange={this.handleChange} />
+          </label>
+        </fieldset>
+        <fieldset>
+          <legend>Terceira parte</legend>
+          <label>
+            Teste agora
+            <input type="checkbox" name="testando" onChange={this.handleChange} />
+          </label>
+          <label>
+            Mensagem
+            <textarea name="message" onChange={this.handleChange} />
+          </label>
+          <label>
+            Envie um arquivo
+            <input type="file" />
+          </label>
+        </fieldset>
       </form>
     )
   }
